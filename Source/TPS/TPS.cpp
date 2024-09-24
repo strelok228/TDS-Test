737abcabcc89e90a9d251e4cfc2d8e329bc28138
strelok228 <48530770+strelok228@users.noreply.github.com>
2024-09-24 18:14:35 +0400

    Version

---
+++ Source/TPS/TPS.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "TPS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TPS, "TPS" );
+
+DEFINE_LOG_CATEGORY(LogTPS)
+ 
---
+++ Source/TPS/TPSGameMode.cpp
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "TPSGameMode.h"
+#include "TPSPlayerController.h"
+#include "TPSCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ATPSGameMode::ATPSGameMode()
+{
+	// use our custom PlayerController class
+	PlayerControllerClass = ATPSPlayerController::StaticClass();
+
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprint/Character/TopDownCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
